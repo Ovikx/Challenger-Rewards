83da9287beaefa2f74bcf1cbe4a74162ed7b99ba
Ovik <47368700+Ovikx@users.noreply.github.com>
2021-06-01 10:24:04 -0400

    improved iteration speed and added comments

--- affiliate.py
+++ affiliate.py
@@ -1,5 +1,4 @@
 import discord
-from decimal import *
 from discord.ext import tasks, commands
 import discord.utils
 import os
@@ -11,6 +10,7 @@ intents.typing = False
 intents.presences = True
 intents.members = True
 
+# .env setup
 try:
     s3 = S3Connection(os.environ['AFFILIATE_BOT_TOKEN'])
     print('loaded config vars')
@@ -19,14 +19,18 @@ except:
     print('loaded local env vars')
 
 token = os.getenv('AFFILIATE_BOT_TOKEN')
-
 bot = commands.Bot(command_prefix='r.', case_insensitive=True, chunk_guilds_at_startup=False, intents=intents)
 bot.remove_command('help')
-ovikID = 219966414486896640
-affiliate_links = [
+
+owner_id = 219966414486896640
+guild_id = 574688313919799327
+reward_role_id = 806516503565959188
+
+# The links people can put to get the reward role
+links = [
         'https://top.gg/bot/576181953372356638',
         'https://dsc.gg/challenger',
-        'https://bots.discordlabs.org/bot/challenger'
+        'https://bots.discordlabs.org/bot/challenger',
     ]
 
 @bot.event
@@ -34,21 +38,21 @@ async def on_ready():
     print(f"{bot.user} logged into {len(bot.guilds)} servers")
     game = discord.Game(f'r.help | Rewarding supporters!')
     await bot.change_presence(status=discord.Status.online, activity=game)
-    guild = bot.get_guild(574688313919799327)
-    await guild.chunk()
-
+    await (bot.get_guild(guild_id)).chunk()
     if not affiliate_program.is_running():
         affiliate_program.start()
 
+# Gives the reward role to whoever has one of the links in their status
 @tasks.loop(seconds=120)
 async def affiliate_program():
-    guild = bot.get_guild(574688313919799327)
+    guild = bot.get_guild(guild_id)
+    role = guild.get_role(reward_role_id)
     for member in set(guild.members):
-        if bool([v for v in affiliate_links if v in str(member.activity)]):
-            await member.add_roles(guild.get_role(806516503565959188))
-            print(f'{member.name} has been given Premium!')
+        if bool([v for v in links if v in str(member.activity)]):
+            await member.add_roles(role)
+            print(f'{member.name} has been given Premium')
         else:
-            await member.remove_roles(guild.get_role(806516503565959188))
+            await member.remove_roles(role)
 
 @bot.command()
 async def ping(ctx):
@@ -57,9 +61,9 @@ async def ping(ctx):
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title='What is this bot?', description='This bot rewards free **Challenger Premium** to users who have <@576181953372356638>\'s invite link in their custom status. [Read more about Premium here](https://www.patreon.com/ovikx?fan_landing=true)', color=discord.Color.blurple())
-    embed.add_field(name='Put one of these links in your status to get Premium', value='\n'.join([f'`{i+1}` [{v}]({v})' for i, v in enumerate(affiliate_links)]))
+    embed.add_field(name='Put one of these links in your status to get Premium', value='\n'.join([f'`{i+1}` [{v}]({v})' for i, v in enumerate(links)]))
     embed.add_field(name='Important Notes', value='- Premium is removed when the user removes the link from their status\n- It may take up to two minutes to receive a free Premium subscription', inline=False)
-    embed.add_field(name='Suggest links!', value=f'If you want to add a link to the list above, please DM `{bot.get_user(ovikID)}`.')
+    embed.add_field(name='Suggest links!', value=f'If you want to add a link to the list above, please DM `{bot.get_user(owner_id)}`.')
     await ctx.send(embed=embed)
 
 bot.run(token)
